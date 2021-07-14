34190992a4f5e8229bcc0c562aadcf3027474e5e
Андрей Кривонос <70647515+Krivonos-Andrew@users.noreply.github.com>
2021-07-14 13:21:48 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
-// import getOffers from './data.js';
-// import {
-//   showCard
-// } from './card.js';
+import getOffers from './data.js';
+import {
+  showCard
+} from './card.js';
 import {
   removeDisabledFildset,
   setAddress
 } from './form.js';
 // showCard();
-// const offers = getOffers();
-// showCard(offers[0]);
+const offers = getOffers();
+showCard(offers[0]);
 removeDisabledFildset();
 setAddress(20, 30);
 
@@ -16,7 +16,9 @@ import {
   map,
   mainPinIcon,
   mainPinMarker,
+  putMarkerOnMap
 } from './map.js';
-map();
-mainPinIcon();
-mainPinMarker();
+// map();
+// mainPinIcon();
+// mainPinMarker();
+// putMarkerOnMap();
--- js/map.js
+++ js/map.js
@@ -1,3 +1,7 @@
+import {
+  showCard
+} from './card.js';
+
 const map = L.map('map-canvas')
   .on('load', () => {
     // eslint-disable-next-line no-console
@@ -14,57 +18,55 @@ L.tileLayer(
   },
 ).addTo(map);
 
-const points = [{
-    title: 'Футура',
-    lat: 59.96925,
-    lng: 30.31730,
-  },
-  {
-    title: 'Шаверма',
-    lat: 59.96783,
-    lng: 30.31258,
-  },
-  {
-    title: 'Франк',
-    lat: 59.95958,
-    lng: 30.30228,
-  },
-  {
-    title: 'Ginza',
-    lat: 59.97292,
-    lng: 30.31982,
-  },
-];
+const offersGroup = L.layerGroup().addTo(map);
 
-points.forEach(({
-  lat,
-  lng,
-}) => {
-  const icon = L.icon({
-    iconImage: 'src = "img/pin.svg"',
+const putMarkerOnMap = ((offer) => {
+  const iconOffer = L.icon({
+    iconUrl: 'img/pin.svg',
     iconSize: [40, 40],
     iconAnchor: [20, 40],
   });
-
-  const marker = L.marker({
-    lat,
-    lng,
+  const markerOffer = L.marker({
+    lat: offer.location.lat,
+    lng: offer.location.lng,
   }, {
-    icon,
+    iconOffer,
   });
-
-  marker.addTo(map).bindPopup(points.title);
+  markerOffer.addTo(offersGroup).bindPopup(showCard(offer), {
+    keepInView: true,
+  });
 });
 
+
+// putMarkerOnMap.forEach(({
+//   lat,
+//   lng,
+// }) => {
+//   const icon = L.icon({
+//     iconImage: 'src = "img/pin.svg"',
+//     iconSize: [40, 40],
+//     iconAnchor: [20, 40],
+//   });
+
+//   const marker = L.marker({
+//     lat,
+//     lng,
+//   }, {
+//     icon,
+//   });
+
+//   marker.addTo(map).bindPopup(points.title);
+// });
+
 const mainPinIcon = L.icon({
-  iconImage: 'src = img/main-pin.svg',
+  iconUrl: 'img/main-pin.svg',
   iconSize: [52, 52],
   iconAnchor: [26, 52],
 });
 
 const mainPinMarker = L.marker({
-  lat: 59.96831,
-  lng: 30.31748,
+  lat: 35.41,
+  lng: 139.41,
 }, {
   draggable: true,
   icon: mainPinIcon,
@@ -75,10 +77,11 @@ mainPinMarker.on('moveend', (evt) => {
   // eslint-disable-next-line no-console
   console.log(evt.target.getLatLng());
 });
-mainPinMarker.remove();
+
 
 export {
   map,
   mainPinIcon,
-  mainPinMarker
+  mainPinMarker,
+  putMarkerOnMap
 };
